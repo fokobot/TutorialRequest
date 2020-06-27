02059233f118bc4da84b37019f38da8463137589
Fabian Osorio Iriarte <fabian.osorio.990628@gmail.com>
2020-06-27 12:42:40 -0500

    Added function add request

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp();
 //auth triggers (new user signup)
 exports.newUserSignUp = functions.auth.user().onCreate(user => {
     //for background triggers you must return a value/promise
-    return admin.firestore().collection('users').doc(user.id).set({
+    return admin.firestore().collection('users').doc(user.uid).set({
         email: user.email,
         upvotedOn: [],
     });
@@ -14,6 +14,27 @@ exports.newUserSignUp = functions.auth.user().onCreate(user => {
 //auth triggers (user deleted)
 exports.userDeleted = functions.auth.user().onDelete(user => {
     //for background triggers you must return a value/promise
-    const doc = admin.firestore().collection('users').doc(user.id);
+    const doc = admin.firestore().collection('users').doc(user.uid);
     return doc.delete();
 });
+
+
+// http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+    if (!context.auth) {
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'only authenticated users can add requests'
+        );
+    }
+    if (data.text > 30) {
+        throw new functions.https.HttpsError(
+            'invalid-argument',
+            'request must be no more than 30 characters long'
+        );
+    }
+    return admin.firestore().collection('requests').add({
+        text: data,
+        upvotes: 0,
+    })
+})
